94b5a2adf750e05966a6fe36c41e3e22c0c589a8
hfutgf <beretax308@gmail.com>
2024-02-18 02:05:17 +0500

    feat: added jwt and connected with db

---
+++ src/auth/decorators/user.decorator.ts
@@ -0,0 +1,10 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import type { User } from 'prisma/generated/client';
+
+export const CurrentUser = createParamDecorator(
+  (data: keyof User, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    return data ? user[data] : user;
+  },
+);
